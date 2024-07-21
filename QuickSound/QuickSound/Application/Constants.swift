8b68a23f1d244d38ea695acfa3165332b969bedb
catiz24 <catiz@tutamail.com>
2024-07-22 00:06:01 +0300

    Improve accessibility of UI

---
+++ QuickSound/QuickSound/Application/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  QuickSound
+//
+//  Created by Thomas Di Meco on 22/03/2016.
+//  Copyright © 2016 Thomas Di Meco. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let updatePlistURL = URL(string: "https://raw.githubusercontent.com/tdimeco/QuickSound/master/QuickSound/Info.plist")!
+    static let updatesPageURL = URL(string: "https://github.com/tdimeco/QuickSound/releases")!
+    static let matomoURL = URL(string: "https://analytics.tdimeco.fr/matomo.php")!
+    static let matomoSiteId = "4"
+}
